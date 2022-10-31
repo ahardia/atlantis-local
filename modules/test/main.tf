549591fcd4fc68099753e524fd929a8569b499ed
Akash Hardia <ahardia@ext.sephora.co.in>
2022-10-31 21:41:35 +0530

    poc: tfsec

--- modules/test/main.tf
+++ modules/test/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "example" {}
 
-# resource "aws_instance" "example" {
-#   ami           = var.ami
-#   instance_type = var.instance_type
-# }
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+}
